8a1a5a4c0b591a5885b50a73e2d3e4f60392e5f9
WHloki <1845199691@qq.com>
2020-03-13 19:16:27 +0800

    Site updated: 2020-03-13 19:16:26

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function ($) {
     $("body").click(function (e) {
         var a = new Array
-            ("��ǿ", "����", "����", "��г", "����", "ƽ��", "����", "����", "����", "��ҵ", "����", "����");
+            ("I", "L", "O", "V", "E", "Y", "O", "U","!");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
